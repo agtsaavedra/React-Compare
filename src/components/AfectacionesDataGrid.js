df0d0405044b41d6253efde81bab36c8d125adad
agustinsaavedra <agustinsaavedra@ufasta.edu.ar>
2024-09-23 10:46:34 -0300

    sincronizacion de tablas en filtrado, optiimizacion codigo utils

--- src/components/AfectacionesDataGrid.js
+++ src/components/AfectacionesDataGrid.js
@@ -1,8 +1,8 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { DataGrid, gridFilteredSortedRowIdsSelector, useGridApiRef } from '@mui/x-data-grid';
+import { DataGrid, useGridApiRef } from '@mui/x-data-grid';
 import { Box } from '@mui/material';
-import PartialLoader from './PartialLoader'; // El nuevo loader parcial
-import { calculateTotalWidth, getPinnedColumns, getComparisonColumns } from '../utils/columnUtils';
+import PartialLoader from './PartialLoader';
+import {calculateTotalWidth,getPinnedColumns,getComparisonColumns,getRowsWithIds,syncScroll,handleFilterModelChange,} from '../utils/columnUtils';
 import TooltipFab from './TooltipFab';
 import PaginationComponent from './PaginationComponent';
 import HeaderBoxes from './HeaderBoxes';
@@ -14,18 +14,17 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
   const grid2Ref = useRef(null);
   const pinnedGridRef = useRef(null);
   const [page, setPage] = useState(1);
-  const rowHeight = 45; // Establece la altura de la fila en 45px
+  const rowHeight = 45;
   const [pageSize, setPageSize] = useState(null);
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
-  const [filtersActive, setFiltersActive] = useState(false);// Ref para guardar los IDs anteriores
-  // Estado para los IDs filtrados en la tabla pinned
   const [filteredIds, setFilteredIds] = useState([]);
-  const previousMaxRows = useRef(0);
-  // Estado para el ancho del scrollbar
+  const [rowsCount, setRowsCount] = useState(0);
   const [scrollbarWidth, setScrollbarWidth] = useState(0);
-  const [pinnedScrollbarWidth, setPinnedScrollbarWidth] = useState(0); // Para la tabla pinned
+  const [pinnedScrollbarWidth, setPinnedScrollbarWidth] = useState(0);
 
-  const apiRefPinned = useGridApiRef(); // Usar apiRef solo para la tabla pinned
+  const apiRefPinned = useGridApiRef();
+  const apiRefGrid1 = useGridApiRef();
+  const apiRefGrid2 = useGridApiRef();
 
   const calculatedGridHeight = pageSize ? pageSize * rowHeight + 80 : availableHeight;
 
@@ -33,65 +32,44 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
     setPage(newPage);
   };
 
-  const getRowsWithIds = (afectaciones = []) =>
-    afectaciones.map((afectacion, index) => ({
-      id: index + 1,
-      ...afectacion,
-    }));
-
   const allRows1 = getRowsWithIds(afectaciones1);
   const allRows2 = getRowsWithIds(afectaciones2);
   const allPinnedRows = getRowsWithIds(pinnedAfect);
 
   const maxRows = Math.max(allRows1.length, allRows2.length);
-  const [rowsCount, setRowsCount] = useState(maxRows); 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowHeight(window.innerHeight);
-    };
 
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
+  // Actualizar el tamaño de página y el número de filas cuando cambie la altura de la ventana o los filtros
   useEffect(() => {
     const newPageSize = Math.floor((windowHeight - 250) / rowHeight);
     setPageSize(newPageSize > 0 ? newPageSize : 1);
 
-    const maxPages = Math.ceil(maxRows / newPageSize);
-    if (page > maxPages) {
-      setPage(maxPages);
-    }
+    const currentRowsCount = filteredIds.length > 0 ? filteredIds.length : maxRows;
+    setRowsCount(currentRowsCount);
 
-    // Calcula el ancho total de las columnas para sincronizar el ancho del scrollbar
     const totalWidth = calculateTotalWidth(getComparisonColumns(afectaciones1, afectaciones2, 'table1')) + 900;
-    const totalPinnedWidth = calculateTotalWidth(getPinnedColumns(pinnedAfect)) + 900; // Para la tabla pinned
-
+    const totalPinnedWidth = calculateTotalWidth(getPinnedColumns(pinnedAfect)) + 900;
     setScrollbarWidth(totalWidth);
     setPinnedScrollbarWidth(totalPinnedWidth);
-  }, [windowHeight, maxRows, page, afectaciones1, afectaciones2, pinnedAfect]);
+  }, [windowHeight, maxRows, afectaciones1, afectaciones2, pinnedAfect, filteredIds]);
 
-  const paginatedAfectaciones1 = allRows1
-    .filter(row => filteredIds.length === 0 || filteredIds.includes(row.id)) // Mostrar solo las filas que coincidan con los IDs filtrados
-    .slice((page - 1) * pageSize, page * pageSize);
+  // Ajustar la página si los filtros modifican el número de páginas disponibles
+  useEffect(() => {
+    const maxPages = Math.ceil(rowsCount / pageSize);
 
-  const paginatedAfectaciones2 = allRows2
-    .filter(row => filteredIds.length === 0 || filteredIds.includes(row.id)) // Mostrar solo las filas que coincidan con los IDs filtrados
-    .slice((page - 1) * pageSize, page * pageSize);
-
-  const paginatedPinnedAfect = allPinnedRows.slice((page - 1) * pageSize, page * pageSize);
-
-  const syncScroll = (scrollLeft) => {
-    if (grid1Ref.current && grid2Ref.current) {
-      grid1Ref.current.querySelector('.MuiDataGrid-virtualScroller').scrollLeft = scrollLeft;
-      grid2Ref.current.querySelector('.MuiDataGrid-virtualScroller').scrollLeft = scrollLeft;
+    if (page > maxPages) {
+      setPage(maxPages > 0 ? maxPages : 1);
     }
-  };
+  }, [rowsCount, pageSize]);
+
+  // Al aplicar un filtro, restablecer la página a la primera y ajustar el número de filas
+  useEffect(() => {
+    const filteredRowCount = filteredIds.length > 0 ? filteredIds.length : maxRows;
+    setPage(1); // Restablecer siempre a la primera página al aplicar un filtro
+    setRowsCount(filteredRowCount); // Ajustar el número de filas basado en los filtros
+  }, [filteredIds]);
 
   const handleScroll = (e) => {
-    syncScroll(e.target.scrollLeft);
+    syncScroll(e.target.scrollLeft, grid1Ref, grid2Ref);
   };
 
   const handlePinnedScroll = (e) => {
@@ -100,75 +78,22 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
     }
   };
 
-  const handleClearFilterPinned = () => {
-    if (filteredIds.length > 0) {
-      setFilteredIds([]); // Restablecemos los IDs filtrados
-      if (grid1Ref.current) {
-        grid1Ref.current.apiRef?.current.forceUpdate(); // Forzamos la actualización de la tabla 1
-      }
-      if (grid2Ref.current) {
-        grid2Ref.current.apiRef?.current.forceUpdate(); // Forzamos la actualización de la tabla 2
-      }
-    }
-  };
+  const paginatedAfectaciones1 = allRows1
+    .filter((row) => filteredIds.length === 0 || filteredIds.includes(row.id))
+    .slice((page - 1) * pageSize, page * pageSize);
 
+  const paginatedAfectaciones2 = allRows2
+    .filter((row) => filteredIds.length === 0 || filteredIds.includes(row.id))
+    .slice((page - 1) * pageSize, page * pageSize);
 
-  const handleFilterModelChange = (model) => {
-    setTimeout(() => {
-      const visibleRowIds = gridFilteredSortedRowIdsSelector(apiRefPinned.current.state);
-      
-      // Verificamos si model.items existe y tiene al menos un elemento
-      if (model.items && model.items.length > 0) {
-        // Si el valor del primer filtro es vacío, nulo o no definido, vaciamos los filteredIds
-        if (model.items[0].value === '' || model.items[0].value === null || !model.items[0].value) {
-          setFilteredIds([]); // Vaciar filteredIds si no hay valor en el filtro
-        } else {
-          setFilteredIds(visibleRowIds); // Actualizamos los IDs filtrados si hay un valor válido
-        }
-      } else {
-        // Si model.items está vacío o no existe, también vaciamos filteredIds
-        setFilteredIds([]); // Vaciar filteredIds si no hay filtros activos
-      }
-    }, 50); // Timeout para asegurar que el filtro se aplique antes de capturar los IDs
-  };
-  
-
-  useEffect(() => {
-
-    console.log(previousMaxRows.current)
-    // Guardar el valor original de maxRows antes de aplicar los filtros
-    if (filteredIds.length > 0 && previousMaxRows.current === 0) {
-      previousMaxRows.current = maxRows; // Guardamos el número total de filas antes de aplicar los filtros
-    }
-  
-    if (filteredIds.length === 0) {
-      // Restaurar el valor original de maxRows y recalcular el pageSize
-      setRowsCount(previousMaxRows.current); // Restaurar el número total de filas sin filtrar
-    } else {
-      // Actualizar el número total de filas filtradas
-      setRowsCount(filteredIds.length); // Actualizamos el estado con el número de filas filtradas
-    }
-  }, [filteredIds, maxRows]);
-  
-  useEffect(() => {
-    if (!filtersActive) {
-      // Si no hay filtros activos, restauramos el número original de filas
-      if (previousMaxRows.current === 0) {
-        previousMaxRows.current = maxRows; // Guardamos el número total de filas la primera vez
-      }
-      setRowsCount(previousMaxRows.current); // Restauramos el número de filas sin filtrar
-    } else {
-      // Si hay filtros activos, actualizamos el número de filas filtradas
-      setRowsCount(filteredIds.length);
-    }
-  }, [filtersActive, filteredIds.length, maxRows]);
-
+  const paginatedPinnedAfect = allPinnedRows
+    .filter((row) => filteredIds.length === 0 || filteredIds.includes(row.id))
+    .slice((page - 1) * pageSize, page * pageSize);
 
   const pinnedKeys = Object.keys(pinnedAfect[0] || {});
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', position: 'relative', height: 'calc(100vh - 90px - 160px)' }}>
-      {/* TooltipFab actualizado sin la funcionalidad de diferencias */}
       <TooltipFab />
 
       <Box sx={{ display: 'flex', width: '100%', position: 'sticky', zIndex: 1000, boxSizing: 'border-box' }}>
@@ -176,41 +101,38 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
       </Box>
 
       {loading ? (
-        <PartialLoader /> // Usamos el loader parcial aquí
+        <PartialLoader />
       ) : afectaciones1.length > 0 ? (
         <>
-          {/* Contenedor para las tablas */}
           <Box sx={{ display: 'flex', width: '100%', flexGrow: 1 }}>
             <Box sx={{ display: 'flex', width: '100%', flexGrow: 1 }}>
-              {/* Tabla Pinned */}
               <Box ref={pinnedGridRef} sx={{ width: '20%', boxSizing: 'border-box', height: calculatedGridHeight }}>
-                <Box sx={{ display: 'flex', flexGrow: 1, height: calculatedGridHeight }}>
-                  <DataGrid
-                    rows={paginatedPinnedAfect}
-                    columns={getPinnedColumns(pinnedAfect)} // Aquí usamos el renderCell en la columna "Nombre"
-                    pageSize={pageSize}
-                    apiRef={apiRefPinned} // Usar apiRef solo para la tabla pinned
-                    hideFooterPagination
-                    disableSelectionOnClick
-                    autoHeight
-                    disableColumnSorting
-                    rowHeight={rowHeight} // Establecer la altura de la fila a 40px
-                    onFilterModelChange={handleFilterModelChange} // Capturar cambios de filtro en la tabla pinned
-                    sx={{
-                      '& .MuiDataGrid-columnHeaders': { position: 'sticky' },
-                      '& .MuiDataGrid-virtualScroller': { marginTop: '0 !important' },
-                      '& .MuiDataGrid-main': { overflow: 'visible' },
-                      '& .MuiDataGrid-footerContainer': { display: 'none' },
-                      '& .MuiDataGrid-filler': { display: 'none' },
-                    }}
-                    components={{
-                      NoRowsOverlay: CustomNoRowsOverlay, // Mensaje personalizado cuando no hay filas
-                      NoResultsOverlay: CustomNoRowsOverlay,
-                    }}
-                    localeText={esES.components.MuiDataGrid.defaultProps.localeText}
-                  />
+              <Box sx={{ display: 'flex', flexGrow: 1, height: calculatedGridHeight }}>
+                <DataGrid
+                  rows={paginatedPinnedAfect}
+                  columns={getPinnedColumns(pinnedAfect)}
+                  pageSize={pageSize}
+                  apiRef={apiRefPinned}
+                  hideFooterPagination
+                  disableSelectionOnClick
+                  autoHeight
+                  disableColumnSorting
+                  rowHeight={rowHeight}
+                  onFilterModelChange={(model) => handleFilterModelChange(model, apiRefPinned, setFilteredIds, pageSize, setPage, setRowsCount, page)}
+                  sx={{
+                    '& .MuiDataGrid-columnHeaders': { position: 'sticky' },
+                    '& .MuiDataGrid-virtualScroller': { marginTop: '0 !important' },
+                    '& .MuiDataGrid-main': { overflow: 'visible' },
+                    '& .MuiDataGrid-footerContainer': { display: 'none' },
+                    '& .MuiDataGrid-filler': { display: 'none' },
+                  }}
+                  components={{
+                    NoRowsOverlay: CustomNoRowsOverlay,
+                    NoResultsOverlay: CustomNoRowsOverlay,
+                  }}
+                  localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+                />
                 </Box>
-                {/* Scrollbar horizontal para la tabla pinned */}
                 <Box
                   className="scrollbar-container-pinned"
                   sx={{
@@ -228,7 +150,6 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
                 </Box>
               </Box>
 
-              {/* Contenedor para tablas 1 y 2 */}
               <Box
                 sx={{
                   width: '80%',
@@ -240,7 +161,6 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
                 }}
               >
                 <Box sx={{ display: 'flex', flexGrow: 1 }}>
-                  {/* Tabla 1 */}
                   <Box ref={grid1Ref} sx={{ width: '50%', height: calculatedGridHeight }}>
                     <DataGrid
                       rows={paginatedAfectaciones1}
@@ -250,8 +170,9 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
                       hideFooterPagination
                       disableSelectionOnClick
                       disableColumnSorting
-                      disableColumnFilter // Deshabilitar la opción de filtrado en esta tabla
-                      rowHeight={rowHeight} // Establecer la altura de la fila a 40px
+                      rowHeight={rowHeight}
+                      apiRef={apiRefGrid1}
+                      onFilterModelChange={(model) => handleFilterModelChange(model, apiRefGrid1 , setFilteredIds, pageSize, setPage, setRowsCount, page)}
                       sx={{
                         '& .MuiDataGrid-columnHeaders': { position: 'sticky' },
                         '& .MuiDataGrid-virtualScroller': { marginTop: '0 !important' },
@@ -260,14 +181,13 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
                         '& .MuiDataGrid-filler': { display: 'none' },
                       }}
                       components={{
-                        NoRowsOverlay: CustomNoRowsOverlay, // Mostrar mensaje personalizado
+                        NoRowsOverlay: CustomNoRowsOverlay,
                         NoResultsOverlay: CustomNoRowsOverlay,
                       }}
                       localeText={esES.components.MuiDataGrid.defaultProps.localeText}
                     />
                   </Box>
 
-                  {/* Tabla 2 */}
                   <Box ref={grid2Ref} sx={{ width: '50%', height: calculatedGridHeight }}>
                     <DataGrid
                       rows={paginatedAfectaciones2}
@@ -276,9 +196,10 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
                       hideFooterPagination
                       disableSelectionOnClick
                       disableColumnSorting
-                      disableColumnFilter // Deshabilitar la opción de filtrado en esta tabla
                       autoHeight
-                      rowHeight={rowHeight} // Establecer la altura de la fila a 40px
+                      rowHeight={rowHeight}
+                      apiRef={apiRefGrid2}
+                      onFilterModelChange={(model) => handleFilterModelChange(model, apiRefGrid2 , setFilteredIds, pageSize, setPage, setRowsCount, page)}
                       sx={{
                         '& .MuiDataGrid-columnHeaders': { position: 'sticky' },
                         '& .MuiDataGrid-virtualScroller': { marginTop: '0 !important' },
@@ -295,7 +216,6 @@ const AfectacionesDataGrid = ({ afectaciones1 = [], afectaciones2 = [], pinnedAf
                   </Box>
                 </Box>
 
-                {/* Scrollbar horizontal sincronizado para las tablas 1 y 2 */}
                 <Box
                   className="scrollbar-container"
                   sx={{
--- src/utils/columnUtils.js
+++ src/utils/columnUtils.js
@@ -1,3 +1,6 @@
+import { gridFilteredSortedRowIdsSelector } from '@mui/x-data-grid';
+
+
 export const calculateColumnWidth = (field, rows, headerText) => {
     const headerWidth = headerText.length * 10;
     const maxCellWidth = Math.max(...rows.map((row) => String(row[field] || '').length * 10));
@@ -8,8 +11,40 @@ export const calculateColumnWidth = (field, rows, headerText) => {
     return columns.reduce((totalWidth, col) => totalWidth + col.width, 0);
   };
 
-  
-  
+  export const getRowsWithIds = (afectaciones = []) =>
+    afectaciones.map((afectacion, index) => ({
+      id: index + 1,
+      ...afectacion,
+    }));
+
+    export const handleFilterModelChange = (model, apiRef, setFilteredIds, pageSize, setPage, setRowsCount, page) => {
+      setTimeout(() => {
+        const visibleRowIds = gridFilteredSortedRowIdsSelector(apiRef.current.state);
+    
+        if (model.items && model.items.length > 0 && model.items[0].value) {
+          setFilteredIds(visibleRowIds); // Actualizar filteredIds solo si hay un valor en el filtro
+        } else {
+          setFilteredIds([]); // Limpiar filteredIds si no hay filtros activos
+        }
+    
+        // Actualizar la paginación y el conteo de filas inmediatamente después de aplicar los filtros
+        const filteredRowCount = visibleRowIds.length;
+        const maxPages = Math.ceil(filteredRowCount / pageSize);
+        if (page > maxPages) {
+          setPage(1); // Volver a la primera página si el número de filas es menor que la página actual
+        }
+        setRowsCount(filteredRowCount); // Actualizar el conteo de filas con los datos filtrados
+      }, 50);
+    };
+
+    export const syncScroll = (scrollLeft, grid1Ref, grid2Ref) => {
+      if (grid1Ref.current && grid2Ref.current) {
+        grid1Ref.current.querySelector('.MuiDataGrid-virtualScroller').scrollLeft = scrollLeft;
+        grid2Ref.current.querySelector('.MuiDataGrid-virtualScroller').scrollLeft = scrollLeft;
+      }
+    };
+
+
   export const getPinnedColumns = (pinnedAfect) => {
     const pinnedColumnOrder = ['Legajo', 'nombre', 'EnSnap', 'funcion', 'CategoriaLiquidacion', 'idAfectacionAcademica', 'idAfectacionPresupuestaria'];
     
